ab4613cab0f7971bb4bb7526f0c40209d91209fb
Engr. Gentle Inyang <engen.inyang@gmail.com>
2023-06-02 00:04:52 +0100

    Create 10-binary_tree_depth.c
    dept of a tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - lets check the depth
+ * tree: points to the root of the tree
+ * Return: depth of tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+if (tree == NULL || tree->parent == NULL)
+return (0);
+return (1 + binary_tree_depth(tree->parent));
+}
