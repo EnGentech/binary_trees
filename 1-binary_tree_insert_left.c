b3d16c1f8a44c8942da36990503afefe8a9cc080
Engr. Gentle Inyang <engen.inyang@gmail.com>
2023-06-01 22:01:08 +0100

    Update 1-binary_tree_insert_left.c
    modified

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,28 +5,22 @@
 * @value: defined value to be stored
 * Return: return a pointer to null
 */
-
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-  if (parent == NULL)
-  {
-  return (NULL);
-  }
-
-  binary_tree_t *new_node = binary_tree_node(parent, value);
-  if (new_node == NULL)
-  {
-  return (NULL);
-  }
-
-  if (parent->left != NULL)
-  {
-  parent->left->parent = new_node;
-  new_node->left = parent->left;
-  }
-
-  parent->left = new_node;
-
-  return (new_node);
-  }
+if (parent == NULL)
+{
+return (NULL);
+}
+binary_tree_t *new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
+{
+return (NULL);
+}
+if (parent->left != NULL)
+{
+parent->left->parent = new_node;
+new_node->left = parent->left;
+}
+parent->left = new_node;
+return (new_node);
+}
