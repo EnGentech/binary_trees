52f05e1a19e25f013af92d6159c5c42b86aa5205
EnGentech <engen.inyang@gmail.com>
2023-06-01 13:24:08 -0700

    modifired

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,18 +7,19 @@
 * Return: return a pointer to nodes newly created
 */
 
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-    {
-        return NULL;
-    }
+binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
 
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
 
-    return new_node;
+return (new_node);
 }
